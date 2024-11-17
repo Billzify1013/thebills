961d8bb4f498a3c1f9826b86be33ed1570c84ec4
Billzify1013 <ckhajwan@gmail.com>
2024-11-17 18:36:25 +0530

    change i setting

--- hotelmanagementsystem/wsgi.py
+++ hotelmanagementsystem/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotelmanagementsystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thebills.settings')
 
 application = get_wsgi_application()
