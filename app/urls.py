02696607debedc25311bf54d07e97f297a1a8162
Billzify1013 <ckhajwan@gmail.com>
2024-12-10 18:05:10 +0530

    Saving local changes before checkout

--- app/urls.py
+++ app/urls.py
@@ -294,4 +294,3 @@ if settings.DEBUG:
 
 
 
-
--- hotelmanagementsystem/settings.py
+++ hotelmanagementsystem/settings.py
@@ -166,12 +166,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static'),
 ]
