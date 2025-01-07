61383e74aaf09888bca2f409c9b8c37581ca62ad
Billzify1013 <ckhajwan@gmail.com>
2025-01-08 00:57:00 +0530

    changes done

--- hotelmanagementsystem/urls.py
+++ hotelmanagementsystem/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from django.conf.urls import handler404
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('chandanbillzify/admin/', admin.site.urls),
     path("",include('app.urls')),
 ]
 
